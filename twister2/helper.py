c6bd7ac93f51577407853c0b2b50b74e5e1ea2e2
Fundakowski, Lukasz <lukasz.fundakowski@nordicsemi.no>
2022-03-16 16:47:55 +0100

    Update helper

--- twister2/helper.py
+++ twister2/helper.py
@@ -1,9 +1,11 @@
 import pytest
 
+from twister2.yaml_test_class import YamlFunction
+
 
 def is_yaml_test(item: pytest.Item) -> bool:
     """Return True if item is a yaml test."""
-    if hasattr(item.function, 'spec'):
+    if isinstance(item, YamlFunction):
         return True
     else:
         return False
